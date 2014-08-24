93642e78482e0892be267f5aafe15143da0f0fe4
AJR27 <arcenis.rojas@gmail.com>
2014-08-24 02:52:22 -0400

    changes to cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions store the result of a matrix calculation in the cache and check
+## for whether that object already exists in the cache.
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates an empty variable which will store the matrix on which cacheSolve
+## will be called along with other functions to be used in cacheSolve
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <- NULL
+  set <- function(y){
+    x <<- y
+    mat <<- NULL
+  }
+  get <- function() x
+  setMat <- function(s) mat <<- s
+  getMat <- function() mat
+  list(set = set, get = get,
+       setMat = setMat,
+       getMat = getMat)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve first refers to the result of the makeCacheMatrix function to get the subject
+## matrix then checks whether the result of the inverse of that matrix is in the cache, if
+## so, it retrieves and returns it, otherwise, it calculates the inverse.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mat <- x$getMat()
+  if(!is.null(mat)){
+    message("Getting cached data...")
+    return(mat)
+  }
+  data <- x$get()
+  mat <- solve(data, ...)
+  x$setMat(mat)
+  mat
 }
